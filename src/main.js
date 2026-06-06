390c527fdddbbf41976dc83e43968694b3441ba7
Nick Wu <wu2264@purdue.edu>
2026-06-06 15:49:46 -0400

    basic macbook render

--- src/main.js
+++ src/main.js
@@ -1,45 +1,50 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
+
+// Scene Config
+const scene = new THREE.Scene();
+const color = new THREE.Color().setHex(0xfffbdb);
+scene.background = color;
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
+// Renderer Config
 renderer.setSize(w, h);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+const pmrem = new THREE.PMREMGenerator(renderer);
+scene.environment = pmrem.fromScene(new RoomEnvironment()).texture;
+
 document.body.appendChild(renderer.domElement);
 
+// Camera Config
 const fov = 75;
 const aspect = w / h;
 const near = 0.1;
-const far = 10;
+const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 camera.position.z = 2;
-const scene = new THREE.Scene();
 
+// Orbit Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  flatShading: true,
-});
-const mesh = new THREE.Mesh(geo, mat);
-scene.add(mesh);
+// Lighting
+const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+dirLight.position.set(5, 10, 5);
+scene.add(dirLight);
 
-const wireMat = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: true,
-});
-
-const wireMesh = new THREE.Mesh(geo, wireMat);
-wireMesh.scale.setScalar(1.001);
-mesh.add(wireMesh);
-
-const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
-scene.add(hemiLight);
+// Loading the model
+const loader = new GLTFLoader();
+const gltf = await loader.loadAsync("/macbook/mac.glb");
+scene.add(gltf.scene);
 
 function animate() {
   requestAnimationFrame(animate);
